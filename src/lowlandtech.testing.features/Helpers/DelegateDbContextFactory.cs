2eca957d6082777d433301b9dc26db38ce73002e
wendellmva <wendellmva@lowlandtech.com>
2025-07-10 20:46:04 +0200

    chore: implementing workflows

---
+++ src/lowlandtech.testing.features/Helpers/DelegateDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace LowlandTech.Testing.Features.Helpers;
+public class DelegateDbContextFactory<T> : IDbContextFactory<T> where T : DbContext
+{
+    private readonly T _db;
+
+    public DelegateDbContextFactory(T db)
+    {
+        _db = db;
+    }
+
+    public T CreateDbContext() => _db;
+}
