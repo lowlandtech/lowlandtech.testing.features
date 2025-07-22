3ba1b5da941251150f7927a1553dbf66ee9823ae
wendellmva <wendellmva@lowlandtech.com>
2025-07-22 13:46:33 +0200

    feat(#6): add flow routing rules and BDD support
    - Updated solution to include `LowlandTech.Flows.Specs` project for flow routing specifications.
    - Introduced `ScenarioAttribute` for BDD scenario annotations.
    - Enhanced `SpecificationIdAttribute` for better use case documentation.
    - Added `FlowRoutingRuleConfiguration` for configuring flow routing rules in the database.
    - Updated `Flow` class to include a collection of routing rules.
    - Enhanced `FlowRoutingRule` with additional properties and documentation.
    - Introduced `FlowRoutingRuleExtensions` for matching rules against input objects.
    - Updated `FlowContext` to manage flow routing rules.
    - Added global using directives for easier namespace access.
    - Created new feature specifications for routing rules scenarios.
    - Modified `FlowRouter` to support new routing logic.
    - Refactored and added tests for comprehensive coverage of routing rules functionality.
    - Reorganized project structure for clarity and maintainability.
    - Updated seeding logic in `FlowRouterEdgeCaseUseCase` and `FlowRouterUseCase` for testing.
    - Refactored `WhenSelectingFlows` and `WhenSelectingFlowsEdgeCases` to validate new routing rules behavior.

---
+++ src/lowlandtech.testing.features/Attributes/SpecificationIdAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace LowlandTech.Testing.Features.Attributes;
+
+/// <summary>
+/// Specifies a unique identifier for a use case associated with a class.
+/// </summary>
+/// <remarks>This attribute can be applied to a class to associate it with one or more use case identifiers. Use
+/// this attribute to document or categorize classes based on their related use cases.</remarks>
+/// <param name="specificationId">The unique identifier for the use case. This value cannot be null or empty.</param>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class SpecificationIdAttribute(string specificationId) : NodeIdAttribute(specificationId)
+{
+    /// <summary>
+    /// Gets the unique identifier for the use case.
+    /// </summary>
+    public string SpecificationId { get; } = specificationId;
+}
